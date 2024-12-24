701fd9ffb05555578ae0d8774dc0ebeea89ce5fa
Minhajul Islam <mmminhaj221@gmail.com>
2024-12-24 23:09:40 +0600

    added basics of python

---
+++ basics.py
@@ -0,0 +1,14 @@
+# Declaring var 
+name = "Minhaj"
+age = 21
+is_Student = True  # Note : The "T" and "F" must be capital in true and false
+
+# Printing variables
+print(name , age , is_Student)
+
+# Checking type 
+print(type(name) , type(age) , type(is_Student))
+
+# Dynamic string  or f string 
+dynamic_string = f"I am {name} , I am {age} years old"
+print(dynamic_string)
