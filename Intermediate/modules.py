1804a8ed9a647b5cb40e0aced8e1417a5e04d918
Minhajul Islam <mmminhaj221@gmail.com>
2024-12-25 18:04:49 +0600

    added modules

---
+++ Intermediate/modules.py
@@ -0,0 +1,9 @@
+from function import sum
+
+# we can rename the function name using as
+from function import doubleIt as double
+
+# to import all functions frm a perticular file
+from function import * 
+
+print(double(5))
